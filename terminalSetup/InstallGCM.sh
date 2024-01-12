c546acb3a3cb0bd1228b77c708bc15fdff3406e7
alexretana <alex.retana@live.com>
2024-01-12 17:18:36 -0600

    add Install Git Credential Manager script

---
+++ terminalSetup/InstallGCM.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define URLs and paths
+GCM_RELEASE_URL="https://github.com/microsoft/Git-Credential-Manager-for-Windows/releases/download/1.20.0/gcmw-v1.20.0.zip"
+INSTALL_PATH="/usr/lib/git-core"
+
+# Download GCM release zip
+wget "$GCM_RELEASE_URL" -P "$INSTALL_PATH"
+
+# Unzip contents to the installation path
+bsdtar -xf "$INSTALL_PATH/gcmw-v1.20.0.zip" -C "$INSTALL_PATH"
+
+# Cleanup: Remove the downloaded zip file
+rm "$INSTALL_PATH/gcmw-v1.20.0.zip"
+
