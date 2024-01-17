a97adb13c3d852b81165b6a588aab66c7c6f9504
alexretana <alex.retana@live.com>
2024-01-16 20:51:50 -0600

    Fixed bug where init.lua was not sent to right place

--- NUpdateVim.sh
+++ NUpdateVim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 username=$(whoami)
-path="/c/Users/$username/AppData/Local/"
+path="/c/Users/$username/AppData/Local/nvim/init.lua"
 
 rm $path 2>/dev/null
 ln -sf ~/kickstart/nvim/init.lua $path
