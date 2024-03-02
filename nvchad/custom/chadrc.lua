bce0a36eb8eddb7fa4a676f926392c8081f57572
alexretana <alex.retana@live.com>
2024-03-02 16:24:47 -0600

    Save all nvchad configs, core and custom

---
+++ nvchad/custom/chadrc.lua
@@ -0,0 +1,39 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "blossom_light",
+  theme_toggle = { "blossom_light", "sweetpastel" },
+
+  transparency = true,
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌",
+      "▐ ██████   █████               █████████  █████                    █████▌",
+      "▐░░██████ ░░███               ███░░░░░███░░███                    ░░███ ▌",
+      "▐ ░███░███ ░███  █████ █████ ███     ░░░  ░███████    ██████    ███████ ▌",
+      "▐ ░███░░███░███ ░░███ ░░███ ░███          ░███░░███  ░░░░░███  ███░░███ ▌",
+      "▐ ░███ ░░██████  ░███  ░███ ░███          ░███ ░███   ███████ ░███ ░███ ▌",
+      "▐ ░███  ░░█████  ░░███ ███  ░░███     ███ ░███ ░███  ███░░███ ░███ ░███ ▌",
+      "▐ █████  ░░█████  ░░█████    ░░█████████  ████ █████░░████████░░████████▌",
+      "▐░░░░░    ░░░░░    ░░░░░      ░░░░░░░░░  ░░░░ ░░░░░  ░░░░░░░░  ░░░░░░░░ ▌",
+      "▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌",
+    },
+  }
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+
+return M
---
+++ nvchad/custom/mappings.lua
@@ -0,0 +1,63 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+      },
+    ["<leader>tt"] = {
+      function()
+        require("base46").toggle_theme()
+      end,
+      "toggle NvChad theme",
+    },
+    ["<leader>ty"] = {
+      function()
+        require("base46").toggle_transparency()
+      end,
+      "toggle NvChad theme",
+    },
+    ["<C-v>"] = { '"+p', "Paste from OS clipboard"},
+    ["J"] = { "mzJ`z", "Join Lines, keep cursor in place"},
+
+    -- big moves recenter line
+    ["<C-d>"] = { "<C-d>zz", "Move down and recenter"},
+    ["<C-u>"] = { "<C-u>zz", "Move down and recenter"},
+    ["n"] = { "nzzzv", "Next match and recenter"},
+    ["N"] = { "Nzzzv", "Previous match and recenter"},
+
+    -- Option to delete/paste without yanking 
+    ["<leader>d"] = { '"_d', "delete without yanking"},
+
+    -- no accidently closing vim
+    ["Q"] = { "<nop>", "Quiting with Cap 'Q' disabled"},
+
+    -- fancy search and replace on word cursor is on
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+
+    -- move selected lines up and down 
+    ["<A-j>"] = {":m '>+1<CR>gv=gv", "Move Highlighed Lines Up"},
+    ["<A-k>"] = {":m '<-2<CR>gv=gv", "Move Highlighed Lines Down"},
+
+    -- Access to OS Clipboard
+    ["<C-c>"] = { '"+y', "Paste from OS clipboard"},
+    ["<C-v>"] = { '"+p', "Paste from OS clipboard"},
+
+    -- Option to delete/paste without yanking 
+    ["<leader>p"] = { '"_dP', "paste without yanking"},
+    ["<leader>d"] = { '"_d', "delete without yanking"},
+  },
+}
+
+-- more keybinds!
+
+return M
